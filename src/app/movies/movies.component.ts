9fb65529e96fb71d09afb2f703ff6b5111363062
Loik Maniquant <loik.maniquant@amadeus.com>
2016-10-31 11:28:29 +0100

    creer-un-service
    creer-un-service

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,13 +1,20 @@
 import {Component} from '@angular/core';
+import {MovieService} from './movies.service';
 
 @Component({
   selector: 'movies',
   styleUrls: ['./movies.component.css'],
-  templateUrl: './movies.component.html'
+  templateUrl: './movies.component.html',
+  providers: [MovieService]
 })
 export class MoviesComponent {
-
-  dataMovies = ['Rio', 'Rio2', 'Deadpool'];
+  movieService: MovieService;
+  dataMovies: string[];
+  // dataMovies = ['Rio', 'Rio2', 'Deadpool'];
+  constructor (movieService: MovieService) {
+    this.movieService = movieService;
+    this.dataMovies = movieService.getAllMovies();
+  }
 
   addMovie(movieTitle) {
     this.dataMovies.push(movieTitle);
---
+++ src/app/movies/movies.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MovieService {
+  movies: string[];
+
+  getAllMovies() {
+    this.movies = ['Rio', 'Rio2', 'Deadpool'];
+    return this.movies;
+  }
+  
+}
